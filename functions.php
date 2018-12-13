bd6e90cb170b0c85a266513736c9326060179b56
Thimothé Lamoureux <aparadox@gmail.com>
2018-12-13 13:08:26 -0800

    Added Ajax function to retrieve users

--- functions.php
+++ functions.php
@@ -17,3 +17,34 @@ function prevent_attendance_email($args, $email_type) {
 
 	return $args;
 }
+
+
+
+
+//get patrollers name for Bookings and Attendance, TODO: Move to separate plugin
+add_action('wp_ajax_get_patrollers', 'ajax_patrollers');
+function ajax_patrollers() {
+	global $wpdb; //get access to the WordPress database object variable
+
+	//get names of all businesses
+	$name = $wpdb->esc_like(stripslashes($_POST['patroller'])).'%'; //escape for use in LIKE statement
+	$sql = "select ID, display_name 
+		from $wpdb->users
+		where display_name like %s";
+
+	$sql = $wpdb->prepare($sql, $name);
+
+	$results = $wpdb->get_results($sql);
+
+	//copy the business titles to a simple array
+	$patrollers = array();
+	foreach( $results as $r )
+		$patrollers[] = [
+			"value" => $r->ID,
+			"label" => addslashes($r->display_name)
+		];
+
+	echo json_encode($patrollers);
+
+	die(); //stop "0" from being output
+}
