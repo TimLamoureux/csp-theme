fe54601e4683abd06b27951acd9dfb8703793417
Thimothé Lamoureux <aparadox@gmail.com>
2019-01-09 14:46:19 -0800

    Deactivate secondary nav collapsing on mobile devices

--- functions.php
+++ functions.php
@@ -1,5 +1,59 @@
 <?php
 
+/**
+ * Disable mobile menu for secondary navigation
+ */
+add_action( 'wp_enqueue_scripts', 'generate_dequeue_secondary_nav_mobile', 999 );
+function generate_dequeue_secondary_nav_mobile() {
+	wp_dequeue_style( 'generate-secondary-nav-mobile' );
+}
+
+
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+function theme_scripts() {
+	wp_enqueue_script(
+		'csp-theme',
+			get_stylesheet_directory_uri() . '/assets/js/skipatrol.js',
+			array('jquery-ui-autocomplete'),
+			false,
+			true
+	);
+}
+
+
+// TODO: Implement min in all enqueues
+$min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+require_once( get_stylesheet_directory() . '/functions/conditional_enqueue.php' );
+$conditional_enqueue = new Conditional_Enqueuer();
+if ( wp_script_is( 'bp-jquery-cookie', 'enqueued' ) ) {
+	return;
+} else {
+	$conditional_enqueue->add_script(
+		'bp-jquery-cookie',
+		plugin_dir_url( __FILE__ ) . 'assets/js/jquery-cookie.min.js',
+		array( 'jquery' ),
+		false,
+		true,
+		Conditional_Enqueuer::TYPE_PAGE_TEMPLATE,
+		[
+			'page-templates/page-booking-manage.php'
+		]
+	);
+	// Autocomplete
+	$conditional_enqueue->add_script(
+		'jquery-ui-autocomplete',
+		'',
+		null,
+		null,
+		true,
+		Conditional_Enqueuer::TYPE_PAGE_TEMPLATE,
+		[
+			'page-templates/page-booking-manage.php'
+		]
+	);
+}
+
+
 // Modifications to WP_Full_Calendar
 require_once( get_stylesheet_directory() . '/functions/wp-fullcalendar.php' );
 
@@ -9,9 +63,9 @@ require_once( get_stylesheet_directory() . '/functions/menus.php' );
 require_once( get_stylesheet_directory() . '/functions/events-manager.php' );
 
 
-add_filter('ass_send_email_args', 'prevent_attendance_email', 50, 2);
-function prevent_attendance_email($args, $email_type) {
-	if ("bp-ges-single" == $email_type && "new_booking" == $args['activity']->type) {
+add_filter( 'ass_send_email_args', 'prevent_attendance_email', 50, 2 );
+function prevent_attendance_email( $args, $email_type ) {
+	if ( "bp-ges-single" == $email_type && "new_booking" == $args['activity']->type ) {
 		$args['tokens']['recipient.id'] = -1;
 	}
 
@@ -19,32 +73,30 @@ function prevent_attendance_email($args, $email_type) {
 }
 
 
-
-
 //get patrollers name for Bookings and Attendance, TODO: Move to separate plugin
-add_action('wp_ajax_get_patrollers', 'ajax_patrollers');
+add_action( 'wp_ajax_get_patrollers', 'ajax_patrollers' );
 function ajax_patrollers() {
 	global $wpdb; //get access to the WordPress database object variable
 
 	//get names of all businesses
-	$name = $wpdb->esc_like(stripslashes($_POST['patroller'])).'%'; //escape for use in LIKE statement
+	$name = '%' . $wpdb->esc_like( stripslashes( $_POST['patroller'] ) ) . '%'; //escape for use in LIKE statement
 	$sql = "select ID, display_name 
 		from $wpdb->users
-		where display_name like %s";
+		where display_name like '%s'";
 
-	$sql = $wpdb->prepare($sql, $name);
+	//$sql = $wpdb->prepare( $sql, $wpdb->esc_like($name) );
+	$sql = $wpdb->prepare( $sql, $name );
 
-	$results = $wpdb->get_results($sql);
+	$results = $wpdb->get_results( $sql );
 
-	//copy the business titles to a simple array
 	$patrollers = array();
-	foreach( $results as $r )
+	foreach ( $results as $r )
 		$patrollers[] = [
 			"value" => $r->ID,
-			"label" => addslashes($r->display_name)
+			"label" => addslashes( $r->display_name )
 		];
 
-	echo json_encode($patrollers);
+	echo json_encode( $patrollers );
 
 	die(); //stop "0" from being output
 }
